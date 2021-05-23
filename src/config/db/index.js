6392df9eb389561ef22a397cd1fc0a485f294522
duynguyen <nguyenquocduy91299@gmail.com>
2021-05-23 23:40:19 +0700

    fixed connect db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const CONNECT_STRING = 'mongodb+srv://dbAdmin:dbAdmin123123%21@%23%21@%23@advance-web.b5qbo.mongodb.net/Blog?retryWrites=true&w=majority';
-const CONNECT_STRING = 'mongodb://localhost:27017/demo_blog_dev'
+const CONNECT_STRING = 'mongodb+srv://dbAdmin:dbAdmin123123%21@%23%21@%23@advance-web.b5qbo.mongodb.net/Blog?retryWrites=true&w=majority';
+// const CONNECT_STRING = 'mongodb://localhost:27017/demo_blog_dev'
 async function connect(){
     try {
         await mongoose.connect(CONNECT_STRING, {
